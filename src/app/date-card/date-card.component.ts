07e815cbdfd4c6e1afbb8e5dc2bb0765a452a3fd
shrutanten <shrutanten.work@gmail.com>
2023-08-25 13:00:14 +0530

    Finish solution

--- src/app/date-card/date-card.component.ts
+++ src/app/date-card/date-card.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-date-card',
   templateUrl: './date-card.component.html',
-  styleUrls: ['./date-card.component.scss']
+  styleUrls: ['./date-card.component.scss'],
 })
-export class DateCardComponent {
+export class DateCardComponent implements OnInit, OnDestroy {
+  date = new Date();
+  private dateInterval: ReturnType<typeof setInterval> | null = null;
 
+  ngOnInit(): void {
+    this.dateInterval = setInterval(() => {
+      this.date = new Date();
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.dateInterval!);
+  }
 }
